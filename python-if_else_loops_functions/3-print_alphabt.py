181459c883cbc7178534d083268a5a68173472f1
JuanMartinMena <juanmartinmenac@gmail.com>
2024-09-03 15:26:22 +0000

    alpha

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-for i in range(99):
-    print(f"{i} = 0x{i:x}")
+for i in range(ord('a'), ord('z')+1):
+    if not ord('e') or ord('q'):
+        print("{:c}".format(i), end="")
