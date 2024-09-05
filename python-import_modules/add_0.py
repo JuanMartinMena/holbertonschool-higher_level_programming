135a980e48a946d915cb68c0b2597619daa26762
JuanMartinMena <juanmartinmenac@gmail.com>
2024-09-05 23:07:06 +0000

    add function

---
+++ python-import_modules/add_0.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def add(a, b):
+    """Addition function
+    
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The sum of a and b.
+    """
+    return (a + b)
