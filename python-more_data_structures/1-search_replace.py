b63d9969bf4c3000d2d154b3f051f68352a19f3e
JuanMartinMena <juanmartinmenac@gmail.com>
2024-09-10 14:52:18 +0000

    search_replace

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
+    new_list = []
     for i in my_list:
-        if my_list[i] == search:
-            my_list[i] == replace
-    return my_list
+        if i == search:
+# Si se encuentra el elemento a cambiar se remplaza por replace
+            new_list.append(replace) 
+        else:
+# Si el elemento no se encuentra se agrega el que estaba
+            new_list.append(i)
+    return new_list
