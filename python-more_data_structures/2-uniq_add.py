cc56987377e7ea5d2102c7fe99440702d294506d
JuanMartinMena <juanmartinmenac@gmail.com>
2024-09-10 15:13:38 +0000

    fix logic

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -2,10 +2,6 @@
 def uniq_add(my_list=[]):
     new_list =[]
     for i in my_list: # recorre my_list
-        for j in new_list: # recorre new_list
-            if i == j: # Si se encuentra que existe en la nueva lista
-                break
-            else:
-                pass
-        new_list.append(i)
+        if i not in new_list:
+            new_list.append(i)
     return sum(new_list)
