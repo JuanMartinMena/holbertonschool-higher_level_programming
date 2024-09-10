6b91adf9580b8811e59a734705a1085a5720c97f
JuanMartinMena <juanmartinmenac@gmail.com>
2024-09-10 14:54:42 +0000

    correct pysco style

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -3,9 +3,9 @@ def search_replace(my_list, search, replace):
     new_list = []
     for i in my_list:
         if i == search:
-# Si se encuentra el elemento a cambiar se remplaza por replace
-            new_list.append(replace) 
+            # Si se encuentra el elemento a cambiar se remplaza por replace
+            new_list.append(replace)
         else:
-# Si el elemento no se encuentra se agrega el que estaba
+            # Si el elemento no se encuentra se agrega el que estaba
             new_list.append(i)
     return new_list
