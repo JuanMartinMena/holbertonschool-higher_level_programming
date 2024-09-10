b2aa45366325696f11f9e2c7b722fc63e09cf5e5
JuanMartinMena <juanmartinmenac@gmail.com>
2024-09-10 18:56:16 +0000

    better comments

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -2,10 +2,11 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return None
-    # Se asigna el valor de la primera key a best_key y el primer valor a best_value
     # items devuelve los pares key-value de a_dictionary
-    # iter hace un objeto iterable en iterador esto es para poder recorrer el diccionario
-    # next agarra la primera key y la iguala a best_key y el primer valor y lo iguala a best_value
+    # iter hace un objeto iterable en iterador
+    # esto es para poder recorrer el diccionario
+    # next agarra el siguiente key y valor
+    # en este caso es el primer key y valor
     best_key, best_value = next(iter(a_dictionary.items()))
     for key, value in a_dictionary.items():
         if value > best_value:
