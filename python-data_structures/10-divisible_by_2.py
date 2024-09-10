af89c3d32d26e38239d8d79b16b4b4b428583470
JuanMartinMena <juanmartinmenac@gmail.com>
2024-09-10 12:41:39 +0000

    logic fixed

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    new_list = len(my_list)
-    for i in my_list:
-        if i % 2 == 0:
-            new_list[i] = True
-        else:
-            new_list[i] = False
-    return new_list
+    return [num % 2 == 0 for num in my_list]
