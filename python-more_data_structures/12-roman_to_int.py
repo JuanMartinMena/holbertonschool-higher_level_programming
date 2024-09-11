7570b0ae919a2da1f3f982ec466eba36005f2cb2
JuanMartinMena <juanmartinmenac@gmail.com>
2024-09-11 13:15:56 +0000

    correct psyco style

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -7,7 +7,7 @@ def roman_to_int(roman_string):
         return 0
     if not roman_string:
         return None
-    valores = {'I': 1,'V': 5,'X': 10,'L': 50,'C': 100,'D': 500,'M': 1000}
+    valores = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     total = 0
     valor_anterior = 0
     for simbolo in roman_string:
