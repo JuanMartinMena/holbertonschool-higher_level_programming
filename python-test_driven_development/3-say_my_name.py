5c3eefb47fb44d474535711277a63aeaee9d225a
JuanMartinMena <juanmartinmenac@gmail.com>
2024-09-15 23:41:06 +0000

    white space psyco style

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -4,7 +4,7 @@ Module: 3-say_my_name
 
 This module contains
 a single function that prints the name provided by the user.
-The function ensures 
+The function ensures
 that both the first name and last name are strings before
 printing the formatted name
 
