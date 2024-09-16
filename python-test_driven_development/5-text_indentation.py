3d591ddaac97e6d235cbfe5c81bd41303c56e0a9
JuanMartinMena <juanmartinmenac@gmail.com>
2024-09-16 00:35:08 +0000

    text_identation

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+5-text_indentation.py
+
+Contains the `text_indentation` function that processes and prints a text with 
+two new lines after each '.', '?', and ':'.
+"""
+def text_indentation(text):
+    """
+    Prints text with 2 new lines after each '.', '?', and ':'.
+    
+    Args:
+        text (str): The text to process.
+        
+    Raises:
+        TypeError: If `text` is not a string.
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    
+    i = 0
+    length = len(text)
+    
+    while i < length:
+        print(text[i], end="")
+        
+        if text[i] in {'.', '?', ':'}:
+            print("\n")
+            while i + 1 < length and text[i + 1] == ' ':
+                i += 1
+                if i + 1 < length and text[i + 1] not in {'.', '?', ':'}:
+                    print("\n", end="")
+        i += 1
