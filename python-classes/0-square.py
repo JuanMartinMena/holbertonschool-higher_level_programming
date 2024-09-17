042cd1e614894991185bd331d948dcf2e3c6521a
JuanMartinMena <juanmartinmenac@gmail.com>
2024-09-17 16:14:14 +0000

    documented

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
+"""
+Este módulo define una clase vacía `Square` que representa un cuadrado.
+"""
+
+
 class Square:
+    """
+    Clase vacía que define un cuadrado.
+
+    Actualmente, esta clase no tiene atributos ni métodos.
+    """
     pass
