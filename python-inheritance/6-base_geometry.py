f7765ccf3c626dbac0b89c7352192bd00e8b369b
JuanMartinMena <juanmartinmenac@gmail.com>
2024-09-25 15:01:00 +0000

    6-geo

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Este módulo define una clase BaseGeometry.
+"""
+
+class BaseGeometry:
+    """
+    Clase BaseGeometry que define métodos de geometría.
+    """
+
+    def area(self):
+        """
+        Método que calcula el área.
+        Lanza una excepción indicando que el método no está implementado.
+        """
+        raise Exception("area() is not implemented")
