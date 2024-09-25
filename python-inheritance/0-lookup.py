c0f106cfd60cbdc8c1e19caa6c60b83cab8b4034
JuanMartinMena <juanmartinmenac@gmail.com>
2024-09-25 11:58:14 +0000

    documented

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,3 +1,18 @@
 #!/usr/bin/python3
+"""
+Este módulo define la función lookup, que devuelve
+la lista de atributos y métodos disponibles de un objeto.
+"""
+
+
 def lookup(obj):
+    """
+    Devuelve una lista de los atributos y métodos disponibles del objeto dado.
+
+    Args:
+        obj: El objeto a inspeccionar.
+
+    Returns:
+        list: Una lista de los atributos y métodos disponibles para el objeto.
+    """
     return dir(obj)
--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,4 +1,22 @@
-#!/usr/bin/phyton3
+#!/usr/bin/python3
+"""
+Este módulo define una clase MyList que hereda de la clase list,
+e incluye un método para imprimir la lista ordenada en orden ascendente.
+"""
+
+
 class MyList(list):
+    """
+    Clase que extiende la funcionalidad de la clase incorporada list.
+
+    Métodos:
+        print_sorted(self): Imprime la lista en orden ascendente.
+    """
+
     def print_sorted(self):
+        """
+        Imprime la lista actual, pero en orden ascendente.
+
+        La lista original no se modifica, solo se imprime una versión ordenada.
+        """
         print(sorted(self))
