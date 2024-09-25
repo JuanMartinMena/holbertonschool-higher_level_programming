c9acb5e5ad51e7ee31f544c8818dead116b2c564
JuanMartinMena <juanmartinmenac@gmail.com>
2024-09-25 15:24:05 +0000

    7-geo

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+Este módulo define una clase BaseGeometry.
+"""
+
+
+class BaseGeometry:
+    """
+    Clase BaseGeometry que define métodos de geometría.
+    """
+
+    def area(self):
+        """
+        Método que calcula el área.
+        Lanza una excepción indicando que el método no está implementado.
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        Valida que el valor sea un entero mayor que 0.
+
+        Args:
+            name: El nombre del parámetro a validar.
+            value: El valor a validar.
+
+        Raises:
+            TypeError: Si value no es un entero.
+            ValueError: Si value es menor o igual a 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
