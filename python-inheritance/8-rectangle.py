b2a34860ee717e251747c619b1560ad121d75688
JuanMartinMena <juanmartinmenac@gmail.com>
2024-09-29 22:43:11 +0000

    8-rec

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -3,6 +3,33 @@
 This module defines a Rectangle class that inherits from BaseGeometry.
 """
 
+class BaseGeometry:
+    """
+    A base class for geometric shapes.
+    """
+
+    def area(self):
+        """
+        Raises an Exception with the message that area is not implemented.
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        Validates that a given value is an integer greater than zero.
+
+        Args:
+            name (str): The name of the parameter to validate.
+            value (int): The value to validate.
+
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is less than or equal to 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
 
 class Rectangle(BaseGeometry):
     """
@@ -17,22 +44,27 @@ class Rectangle(BaseGeometry):
             width (int): The width of the rectangle.
             height (int): The height of the rectangle.
         """
-        self.__integer_validator("width", width)
-        self.__integer_validator("height", height)
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
         self.__width = width
         self.__height = height
 
-    def __integer_validator(self, name, value):
+    def __str__(self):
         """
-        Validates that value is a positive integer.
-
-        Args:
-            name: The name of the parameter to validate.
-            value: The value to validate.
-
-        Raises:
-            TypeError: If value is not an integer.
-            ValueError: If value is less than or equal to 0.
+        Returns a string representation of the Rectangle instance.
         """
-        super().integer_validator(name, value)
+        return f"[Rectangle] {self.__width}/{self.__height}"
 
+    @property
+    def width(self):
+        """
+        Returns the width of the rectangle.
+        """
+        return self.__width
+
+    @property
+    def height(self):
+        """
+        Returns the height of the rectangle.
+        """
+        return self.__height
