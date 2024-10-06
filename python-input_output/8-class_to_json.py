239e5239d6e4f3d06d8b2623e8980aaeed31dcae
JuanMartinMena <juanmartinmenac@gmail.com>
2024-10-06 23:20:30 +0000

    psyco style

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
-"""Function that returns the dictionary description with simple data structure for JSON serialization of an object."""
-
+"""Function that returns the dictionary description with simple data structure
+for JSON serialization of an object."""
 
 def class_to_json(obj):
     """Returns the dictionary description for JSON serialization of an object."""
-    return {key: value for key, value in obj.__dict__.items() if isinstance(value, (list, dict, str, int, bool))}
+    return {
+        key: value 
+        for key, value in obj.__dict__.items()
+        if isinstance(value, (list, dict, str, int, bool))
+    }
