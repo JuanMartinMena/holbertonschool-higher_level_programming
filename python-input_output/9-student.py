98970515e98854b4e7df2f1e486b722f558731be
JuanMartinMena <juanmartinmenac@gmail.com>
2024-10-06 23:21:52 +0000

    documented

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -1,28 +1,20 @@
 #!/usr/bin/python3
-class Student:
-    """
-    A class that defines a student by their first name, last name, and age.
-    """
+"""Module that defines a Student class."""
 
+
+class Student:
+    """A class representing a student."""
 
     def __init__(self, first_name, last_name, age):
-        """
-        Initializes a new Student instance.
-
-        Args:
-            first_name (str): The student's first name.
-            last_name (str): The student's last name.
-            age (int): The student's age.
-        """
+        """Initialize a new student instance."""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-        """
-        Retrieves a dictionary representation of a Student instance.
-
-        Returns:
-            dict: A dictionary containing the attributes of the student.
-        """
-        return self.__dict__
+        """Retrieve a dictionary representation of the Student instance."""
+        return {
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'age': self.age
+        }
