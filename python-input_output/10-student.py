05ad9b8f1200216f4d303d4df0746640ad9bd580
JuanMartinMena <juanmartinmenac@gmail.com>
2024-10-06 23:22:25 +0000

    documented

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,34 +1,27 @@
 #!/usr/bin/python3
+"""Module that defines a Student class."""
+
+
 class Student:
-    """
-    A class that defines a student by their first name, last name, and age.
-    """
+    """A class representing a student."""
 
     def __init__(self, first_name, last_name, age):
-        """
-        Initializes a new Student instance.
-
-        Args:
-            first_name (str): The student's first name.
-            last_name (str): The student's last name.
-            age (int): The student's age.
-        """
+        """Initialize a new student instance."""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """
-        Retrieves a dictionary representation of a Student instance.
-
-        Args:
-            attrs (list, optional): A list of attribute names to retrieve.
-
-        Returns:
-            dict: A dictionary containing the specified attributes of the student
-                  or all attributes if attrs is None.
+        """Retrieve a dictionary representation of the Student instance.
+        
+        If attrs is a list of strings, only attribute names contained in
+        this list are retrieved.
         """
         if attrs is None:
-            return self.__dict__
+            return {
+                'first_name': self.first_name,
+                'last_name': self.last_name,
+                'age': self.age
+            }
         
-        return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
+        return {key: getattr(self, key) for key in attrs if hasattr(self, key)}
