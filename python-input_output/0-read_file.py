a404661b9d0b6e7c44f6f31e91ddfeede569fe1b
JuanMartinMena <juanmartinmenac@gmail.com>
2024-10-06 23:06:07 +0000

    psyco style

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -6,6 +6,7 @@ This module defines a function `read_file` that reads a UTF-8 text
 file and prints its contents to stdout.
 """
 
+
 def read_file(filename=""):
     """
     Reads a text file (UTF-8) and prints it to stdout.
