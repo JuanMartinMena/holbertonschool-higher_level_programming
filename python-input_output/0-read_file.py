9ed882ebef9e20d7ce7854d814d5b816fceaf75a
JuanMartinMena <juanmartinmenac@gmail.com>
2024-10-06 22:42:35 +0000

    documented

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -2,12 +2,9 @@
 def read_file(filename=""):
     """
     Reads a UTF-8 text file and prints its content to stdout.
-
+    
     Args:
         filename (str): The name of the file to read. Defaults to an empty string.
-
-    Returns:
-        None: This function prints the file content and returns nothing.
     """
     with open(filename, 'r', encoding='utf-8') as file:
         print(file.read(), end="")
