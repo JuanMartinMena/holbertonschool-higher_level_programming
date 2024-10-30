2798ed68fc9a0f72e144b0aacaee1a08fa921232
JuanMartinMena <juanmartinmenac@gmail.com>
2024-10-30 19:20:19 +0000

    Readme not empty

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,5 +1,5 @@
--- List privileges for user_0d_1
+-- list privileges for user_0d_1
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 
--- List privileges for user_0d_2
+-- list privileges for user_0d_2
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
