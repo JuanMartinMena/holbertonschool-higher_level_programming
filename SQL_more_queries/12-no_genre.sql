2aedd069dc6a479c396225efb10d6eb84660defe
JuanMartinMena <juanmartinmenac@gmail.com>
2024-10-31 18:27:59 +0000

    12 fix logic

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -1,6 +1,6 @@
 -- Seleccionar todas las series que no tienen un género asociado
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id  -- Asegúrate de usar el nombre correcto
 WHERE tv_show_genres.genre_id IS NULL
-ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+ORDER BY tv_shows.title ASC;
