8e08a1f3b7dba6368978850c977d806c03a335ed
JuanMartinMena <juanmartinmenac@gmail.com>
2024-11-04 00:52:00 +0000

    psyco fixed

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -29,7 +29,9 @@ def main():
     cursor = db.cursor()
 
     # Ejecutar la consulta SQL para seleccionar estados que comienzan con 'N'
-    cursor.execute("SELECT * FROM states WHERE BINARY name LIKE 'N%' ORDER BY id ASC")
+    cursor.execute(
+        "SELECT * FROM states WHERE BINARY name LIKE 'N%' ORDER BY id ASC"
+    )
 
     # Obtener todos los resultados
     results = cursor.fetchall()
