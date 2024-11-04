8a4badc1864a5af78406e8950b75025182dca218
JuanMartinMena <juanmartinmenac@gmail.com>
2024-11-04 00:31:45 +0000

    13 fix?

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,7 @@
--- Seleccionar cada género y la cantidad de shows asociados
 SELECT g.name AS genre, COUNT(s.id) AS number_of_shows
 FROM tv_genres g
 JOIN tv_shows_genres sg ON g.id = sg.genre_id
 JOIN tv_shows s ON sg.show_id = s.id
 GROUP BY g.name
+HAVING COUNT(s.id) > 0
 ORDER BY number_of_shows DESC;
